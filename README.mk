2111726fce62596cb42a7f0f6bc2d393c23fbfdf
Torkel Bjørnson-Langen <torkel@innovatio.no>
2016-02-10 04:20:19 +0100

    added readme

---
+++ README.mk
@@ -0,0 +1,11 @@
+AvtaleGiro™
+===========
+
+Work in progress ...
+
+- - -
+
+Brukerdokumentasjon:
+<http://www.avtalegiro.no/bedrift/Brukerdokumentasjon/Pages/default.aspx>
+
+[System specification (sep. 2015)](http://www.avtalegiro.no/bedrift/Brukerdokumentasjon/Documents/AvtaleGiro%20Systemspesifikasjon(eng)%20v%203.2%20sept%202015.pdf)
